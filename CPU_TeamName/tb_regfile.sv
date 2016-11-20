4e5ee648e53f50d101c09e8f3992224c82c2886e
jayfurz <jfursov@uci.edu>
2016-11-19 22:55:59 -0800

    Update tb_regfile.sv

--- CPU_TeamName/tb_regfile.sv
+++ CPU_TeamName/tb_regfile.sv
@@ -1,35 +1,68 @@
 module tb_top;
 
-logic [5:0] rs, [5:0] rt, [5:0] rd, we, [31:0] wd;
-wire operandA, operandB;
+logic clk;
+logic [5:0] ra1;
+logic [5:0] ra2;
+logic [5:0] wa;
+logic we; 
+logic [31:0] wd;
+wire [31:0] rd1;
+wire [31:0] rd2;
 
 regfile regfile_inst (
-			.ra1(rs),
-			.ra2(rt),
-			.wa(rd),
+			.clk(clk);
+			.ra1(ra1),
+			.ra2(ra2),
+			.wa(wa),
 			.we(we),
-			.wd(wd)
-			.rd1(operandA)
-			.rd2(operandB));
+			.wd(wd),
+			.rd1(rd1),
+			.rd2(rd2));
 
 initial begin
-#10
-we = 1; rs = 0; rt = 1; rd = 2; wd = 10; 
+/*#10ns;
+we=1;
+$display ("we is %b", we);
+#10ns;
+we=0;
+$display ("we is %b", we);
+#10ns;
+we=1;
+$display ("we is %b", we);*/
+/*#10ns;
+clk=1;
+we = 1'b1; rs = 2; rt = 1; rd = 2; wd = 10; 
 $display ("rd1 is %d  rd2 is %d ", operandA, operandB);
-#10
-we = 0; rs = 2; rt = 3; rd = 0; wd = 25; 
-$display ("rd1 is %d  rd2 is %d ", operandA, operandB);
-#10
+#10ns;
+clk=0;
 we = 1; rs = 2; rt = 3; rd = 0; wd = 25; 
 $display ("rd1 is %d  rd2 is %d ", operandA, operandB);
-#10
-we = 0; rs = 2; rt = 0; rd = 1; wd = 50; 
+#10ns;
+clk=1;
+we = 1; rs = 2; rt = 3; rd = 0; wd = 25; 
 $display ("rd1 is %d  rd2 is %d ", operandA, operandB);
-#10
+#10ns;
+clk=0;
+we = 1; rs = 2; rt = 0; rd = 1; wd = 50; 
+$display ("rd1 is %d  rd2 is %d ", operandA, operandB);
+#10ns;
+clk=1;
 we = 1; rs = 2; rt = 3; rd = 1; wd = 50; 
 $display ("rd1 is %d  rd2 is %d ", operandA, operandB);
-#10
-we = 0;
-$display ("rd1 is %d  rd2 is %d ", operandA, operandB);
+#10ns;
+clk=0;
+we = 1;
+$display ("rd1 is %d  rd2 is %d ", operandA, operandB);*/
+#50ns;clk=1'b1;
+we=1;wa=0;wd=11;clk=1;
+$display ("rd1 is %d  rd2 is %d ", rd1, rd2);
+#50ns;
+clk=1'b0;wa=1;wd=11;ra2=1;
+ra1=0;
+$display ("rd1 is %d  rd2 is %d ", rd1, rd2);
 
+#50ns;
+clk=1'b1;
+$display ("rd1 is %d  rd2 is %d ", rd1, rd2);
+end
 endmodule;
