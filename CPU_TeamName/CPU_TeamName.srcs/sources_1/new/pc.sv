dfd871706047e8bdd60204209d150effa8d1f730
bonkieash <haas@uci.edu>
2016-11-22 23:47:00 -0800

    Update pc.sv

--- CPU_TeamName/CPU_TeamName.srcs/sources_1/new/pc.sv
+++ CPU_TeamName/CPU_TeamName.srcs/sources_1/new/pc.sv
@@ -1,17 +1,41 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 11/22/2016 09:05:14 PM
+// Design Name: 
+// Module Name: pc
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
 module pc(
     input clk,
-    input rst,
+    input logic rst,
     output logic [5:0] pout
     );
+        
+        
+   always_ff @(posedge clk) begin
+    if(rst) 
+           pout <= 6'b000000;
+    else
+         pout <= pout +1'b1;     
+    end
+                
+                
     
-     always_ff @(posedge clk) begin
-        if(rst) begin
-        output <= 0;
-        end
-        else begin 
-        output++;
-        end
         
            
-   end  
-    
+endmodule  
