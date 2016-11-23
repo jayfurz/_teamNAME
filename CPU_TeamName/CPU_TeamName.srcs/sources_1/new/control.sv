949a81f626197c8930c77754cd88801d5f5c7adb
jayfurz <jfursov@uci.edu>
2016-11-22 20:49:37 -0800

    Update control.sv

--- CPU_TeamName/CPU_TeamName.srcs/sources_1/new/control.sv
+++ CPU_TeamName/CPU_TeamName.srcs/sources_1/new/control.sv
@@ -24,26 +24,25 @@
 module control(
     input logic [31:0] inst,
     /*input reg r,*/ 
-    output logic [2:0] ALUopsel,
+    output logic [2:0] aluopsel,
     output logic mode,
     output logic alusel,
     output logic memsel,
-    output logic Regwe,
-    output logic Memwe,
+    output logic regwe,
+    output logic memwe,
     output logic [5:0] rs,
     output logic [5:0] rt,
     output logic [5:0] rd,
     output logic [14:0] imm
     );
     
-    parameter [3:0] functioncode;
+    parameter logic [3:0] functioncode;
             
                always_combine begin
                    if(inst[31] == 0) begin                                   //if R-type
                        rs <= inst[30:25];
                        rd <= inst[24:19];
-                       mode <= inst[18];
-                       ALUopsel <= inst[17:15];
+                       functioncode <= inst[18:15];
                        rt <= inst[14:9];
                        imm <= 0;
                        alusel <= 0;
@@ -53,25 +52,41 @@ module control(
                        rs <= inst[30:25];
                        rd <= inst[24:19];
                        rt <= 0;
-                       mode <= inst[18];
-                       ALUopsel <= inst[17:15];
+                       functioncode <= inst[18:15];
                        imm <= inst[14:0];
                        alusel <= 1;
                    end
                    
                       
-                  
+                  //function code will decide what the opsel and mode will be
                  
-                   if(ALUopsel == 4'b0100) begin
+                   if(functioncode == 4'b0100) begin
                        memsel <= 1;
-                       Regwe <= 1;
+                       regwe <= 1;
                        memwe <=0;
+                       mode <= 0;
+                       aluopsel <= 3'b010;
+                       
                    end
                 
-                   if(ALUopsel1 == 4'b0110) begin
+                   if(functioncode == 4'b0110) begin
                        memsel <= 1;
                        memwe <= 1;
-                       Regwe <= 0;
+                       regwe <= 0;
+                       mode <= 0;
+                       aluopsel <= 3'b010;
+                   end
+                   
+                   if(functioncode != 4'b0110 && functioncode != 4'b0100) begin
+                       mode <= functioncode[3];
+                       aluopsel <= functioncode[2:0];
+                       memwe <= 0;
+                          if (functioncode != 4'b1111) begin
+                           regwe <= 0;
+                          end
+                          else begin
+                           regwe <= 1;
+                          end
                    end
                    
                  
